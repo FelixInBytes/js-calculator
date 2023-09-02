dfe59403b4cb54af3a065dec73e097234da2cfd8
Felix Schumacher <felixschumacher@ymail.com>
2023-09-02 19:34:05 +0200

    Added functionality
    - clear functionality
    - added querys for ever button
    - added func to input the the first sring of numbers

--- calc.js
+++ calc.js
@@ -1,55 +1,89 @@
-var operateSolution = 0;
+window.addEventListener("DOMContentLoaded", () => {
+  const numberButtons = document.querySelectorAll("[number]");
+  const operationButtons = document.querySelectorAll("[operation]");
+  const clearButton = document.querySelectorAll("[operation-clear]");
+  const equalsButton = document.querySelectorAll("[operation-equals]");
+  var operateSolution = "";
 
+  //logs
+  console.log(numberButtons);
+  console.log(operationButtons);
+  console.log(clearButton);
+  console.log(equalsButton);
 
+  clearButton.forEach((button) => {
+    button.addEventListener("click", clear);
+    console.log("cleared");
+  });
 
-window.addEventListener("DOMContentLoaded", showScreen);
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      appendNumber(button.innerHTML);
+      updateScreen();
+    });
+  });
 
-
-function showScreen() {
-  document.getElementById("calc-display").innerHTML = operateSolution;
-}
-
-
-
-
-
-function operate( num1,operator, num2) {
-  switch (operator) {
-    case "+":
-      operateSolution = add(num1, num2);
-      break;
-    case "-":
-      operateSolution = sub(num1, num2);
-      break;
-    case "*":
-      operateSolution = multi(num1, num2);
-      break;
-    case "/":
-      operateSolution = div(num1, num2);
-      break;
-    default:
-      break;
+  function updateScreen() {
+    document.getElementById("calc-display").innerHTML = operateSolution;
   }
-  console.log(operateSolution);
-  showScreen(operateSolution);
-}
 
-function add(num1, num2) {
-  var solution = num1 + num2;
-  return solution;
-}
+  function appendNumber(num) {
+    operateSolution += num;
+  }
 
-function sub(num1, num2) {
-  var solution = num1 - num2;
-  return solution;
-}
+  /**
+   * 
+   *function showScreen() {
+    operateSolution = document.getElementsByClassName("btn_nmb").innerHTML;
+    document.getElementById("calc-display").innerHTML = operateSolution;
+    } 
+   * 
+   */
+  // shows number saved in var operateSolution on calculator display
 
-function multi(num1, num2) {
-  var solution = num1 * num2;
-  return solution;
-}
+  function operate(num1, operator, num2) {
+    switch (operator) {
+      case "+":
+        operateSolution = add(num1, num2);
+        break;
+      case "-":
+        operateSolution = sub(num1, num2);
+        break;
+      case "*":
+        operateSolution = multi(num1, num2);
+        break;
+      case "/":
+        operateSolution = div(num1, num2);
+        break;
+      default:
+        break;
+    }
+    console.log(operateSolution);
+    showScreen(operateSolution);
+  }
 
-function div(num1, num2) {
-  var solution = num1 / num2;
-  return solution;
-}
+  function add(num1, num2) {
+    var solution = num1 + num2;
+    return solution;
+  }
+
+  function sub(num1, num2) {
+    var solution = num1 - num2;
+    return solution;
+  }
+
+  function multi(num1, num2) {
+    var solution = num1 * num2;
+    return solution;
+  }
+
+  function div(num1, num2) {
+    var solution = num1 / num2;
+    return solution;
+  }
+
+  function clear() {
+    operateSolution = "";
+    updateScreen();
+  }
+});
